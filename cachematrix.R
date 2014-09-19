faddab90a1988e1b6af4f55eca13427e3c6e61db
Sreedharamurthy K <cacofonyx@gmail.com>
2014-09-19 23:12:57 +0530

    Removed template comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,4 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-## The first function, makeCacheMatrix creates a special
+## The first function, makeCacheMatrix, creates a special
 ## matrix, which is really a list containing a function to
 
 ## set the value of the matrix
